e766ce27f571023557e486a47f8ed50b2cb3e8f7
Albert Chang <achang5@scu.edu>
2014-04-22 15:26:30 -0700

    lab3 inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystuff2::Application.config.secret_key_base = '90ea28bb412d158bb1e657f241dafdd2b4b6b97b14b8a1a6fb4589cec7285239504a9f47530d4dcdd7c299960291c8794ec2808e885c8dc370c253c074bda692'
